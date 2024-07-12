098c941f27f04bbc9d74f4ede3cee4cb9c86759f
Somya Sisodia <s.sisodia2015@gmail.com>
2024-07-12 17:22:29 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import CharacterPage from './pages/CharacterPage';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import Favorites from './components/Favorites/Favorites';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/character/:id" element={<ProtectedRoute component={CharacterPage} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/favorites" element={<ProtectedRoute component={Favorites} />} />
+        </Routes>
+       
+      </Router>
+    </AuthProvider>
   );
 }
 
